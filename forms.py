7b981ffe8777499ffee95913d33a0080c0defbc1
ubrandrew <uberandrew126@gmail.com>
2020-01-26 00:32:46 -0500

    Basic login code

---
+++ forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
