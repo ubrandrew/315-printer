449e14569ecd74b50bb92b8d48090f53b4a8c39e
ubrandrew <uberandrew126@gmail.com>
2020-01-26 21:56:21 -0500

    Adding more views

--- forms.py
+++ forms.py
@@ -1,7 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import PasswordField, SubmitField, TextField, FileField, IntegerField
+from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
+    email = TextField('Email')
     password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class GuestForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class RegistrationForm(FlaskForm):
+    email = TextField('Email')
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class PrintForm(FlaskForm):
+    name = PasswordField('Job Name', validators=[DataRequired()])
+    input_file = FileField('Upload File', validators=[DataRequired()])
+    quantity = IntegerField("Quantity", default=1)
     submit = SubmitField('Submit')
